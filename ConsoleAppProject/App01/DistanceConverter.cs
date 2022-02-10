c005d959ed367b870743074cf74e12ee91199c6d
Derek Peacock <derek.peacock@yahoo.com>
2022-02-10 14:18:08 +0000

    Completed Output Heading

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -1,13 +1,26 @@
-﻿namespace ConsoleAppProject.App01
+﻿using System;
+
+namespace ConsoleAppProject.App01
 {
     /// <summary>
     /// Please describe the main features of this App
     /// </summary>
     /// <author>
-    /// Student Name version 0.1
+    /// Derek Peacock version 0.1
     /// </author>
     public class DistanceConverter
     {
+        public void Run()
+        {
+            OutputHeading();
+        }
 
+        private void OutputHeading()
+        {
+            Console.WriteLine("  ==============================");
+            Console.WriteLine("      App01: Distance Converter ");
+            Console.WriteLine("           by Derek Peacock     ");
+            Console.WriteLine("  ==============================");
+        }
     }
 }
--- ConsoleAppProject/Program.cs
+++ ConsoleAppProject/Program.cs
@@ -1,4 +1,5 @@
-﻿using ConsoleAppProject.App03;
+﻿using ConsoleAppProject.App01;
+using ConsoleAppProject.App03;
 using ConsoleAppProject.Helpers;
 using System;
 
@@ -17,10 +18,16 @@ namespace ConsoleAppProject
         public static void Main(string[] args)
         {
             Console.ForegroundColor = ConsoleColor.Yellow;
-            
-            Console.WriteLine("BNU CO453 Applications Programming 2020-2021!");
+
+            Console.WriteLine();            
+            Console.WriteLine("  BNU CO453 Applications Programming 2020-2021!");
             Console.WriteLine();
-            Console.WriteLine("App01 Distance Converter");
+
+            DistanceConverter app01 = new DistanceConverter();
+            app01.Run();
+
+            Console.ForegroundColor = ConsoleColor.White;
+
         }
     }
 }
