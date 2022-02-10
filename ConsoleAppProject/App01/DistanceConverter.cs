2934502d393b9bec47dff2d9573c39a874dd1532
Derek Peacock <derek.peacock@yahoo.com>
2022-02-10 10:57:56 +0000

    Added features 2,4,5

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -10,6 +10,10 @@ namespace ConsoleAppProject.App01
     /// </author>
     public class DistanceConverter
     {
+        private double miles;
+        
+        private double feet;
+
         public DistanceConverter()
         {
 
@@ -21,14 +25,40 @@ namespace ConsoleAppProject.App01
         public void Run()
         {
             OutputHeading();
+
+            InputMiles();
+            ConvertMilesToFeet();
+            OutputFeet();
+        }
+
+        private void OutputFeet()
+        {
+            Console.WriteLine();
+            Console.WriteLine(" " + miles + " miles is " + feet + " feet");
+            Console.WriteLine();
+        }
+
+        private void ConvertMilesToFeet()
+        {
+            feet = miles * 5280;
+        }
+
+        /// <summary>
+        /// Prompt the user to enter a distance in miles as a double
+        /// </summary>
+        private void InputMiles()
+        {
+            Console.Write(" Please enter the distance in miles > ");
+            string value = Console.ReadLine();
+            miles = Convert.ToDouble(value);
         }
 
         private void OutputHeading()
         {
-            Console.WriteLine("==============================");
-            Console.WriteLine("   App01 Distance Converter   ");
-            Console.WriteLine("       by Derek Peacock       ");
-            Console.WriteLine("==============================");
+            Console.WriteLine(" ==============================");
+            Console.WriteLine("    App01 Distance Converter   ");
+            Console.WriteLine("        by Derek Peacock       ");
+            Console.WriteLine(" ==============================");
         }
     }
 }
