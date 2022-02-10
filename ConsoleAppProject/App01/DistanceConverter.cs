da0cb8be2495533edff033476ee2f264eb659d00
Derek Peacock <derek.peacock@yahoo.com>
2022-02-10 14:26:47 +0000

    Completed features 2,3, and 4

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -10,9 +10,41 @@ namespace ConsoleAppProject.App01
     /// </author>
     public class DistanceConverter
     {
+        private double miles;
+
+        private double feet;
+
         public void Run()
         {
             OutputHeading();
+
+            InputMiles();
+            CalculateFeet();
+            OutputFeet();
+        }
+
+        private void OutputFeet()
+        {
+            Console.WriteLine();
+            Console.WriteLine("  " + miles + " miles is " + feet + " feet");
+            Console.WriteLine();
+        }
+
+        private void CalculateFeet()
+        {
+            feet = miles * 5280;
+        }
+
+        /// <summary>
+        /// Output a message to the user to enter the miles
+        /// and then read it in as a string and convert it tp a double
+        /// </summary>
+        private void InputMiles()
+        {
+            Console.WriteLine();
+            Console.Write("  Please enter your distance as miles > ");
+            string value = Console.ReadLine();
+            miles = Convert.ToDouble(value);
         }
 
         private void OutputHeading()
