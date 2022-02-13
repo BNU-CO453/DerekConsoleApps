4246afa5fc1409177b0cdf35862c7b54e58f4245
Derek Peacock <derek.peacock@yahoo.com>
2022-02-13 19:11:31 +0000

    Enable analyzers

--- ConsoleAppProject/Program.cs
+++ ConsoleAppProject/Program.cs
@@ -18,7 +18,7 @@ namespace ConsoleAppProject
             Console.ForegroundColor = ConsoleColor.Yellow;
 
             Console.WriteLine();
-            Console.WriteLine("BNU CO453 Applications Programming 2020-2021!");
+            Console.WriteLine(" BNU CO453 Applications Programming 2020-2021!");
             Console.WriteLine();
 
             DistanceConverter app01 = new DistanceConverter();
